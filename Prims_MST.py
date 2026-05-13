5e693b890e90af7d439083f5eada907c9629beb4
Anuj Sandeep Wagh <anujwaghnda@gmail.com>
2026-05-14 03:45:50 +0530

    Update Prims_MST.py

--- Prims_MST.py
+++ Prims_MST.py
@@ -1,41 +1,71 @@
-def prim(graph):
-    n = len(graph)
+import java.util.*;
 
-    visited = [False] * n
-    visited[0] = True
+public class PrimEasy {
 
-    cost = 0
+    public static void main(String[] args) {
 
-    print("Edge : Weight")
+        int INF = 9999;
 
-    for _ in range(n - 1):
-        min_w = 9999
-        x = 0
-        y = 0
+        int[][] graph = {
+                {0, 2, 0, 6, 0},
+                {2, 0, 3, 8, 5},
+                {0, 3, 0, 0, 7},
+                {6, 8, 0, 0, 9},
+                {0, 5, 7, 9, 0}
+        };
 
-        for i in range(n):
-            if visited[i]:
-                for j in range(n):
-                    if not visited[j] and graph[i][j] != 0:
-                        if graph[i][j] < min_w:
-                            min_w = graph[i][j]
-                            x = i
-                            y = j
+        int n = graph.length;
 
-        print(x, "-", y, ":", graph[x][y])
-        cost += graph[x][y]
-        visited[y] = True
+        boolean[] vis = new boolean[n];
 
-    print("Total Cost:", cost)
+        vis[0] = true;
 
+        int edges = 0;
 
-# Graph (Adjacency Matrix)
-graph = [
-    [0, 2, 0, 6, 0],
-    [2, 0, 3, 8, 5],
-    [0, 3, 0, 0, 7],
-    [6, 8, 0, 0, 9],
-    [0, 5, 7, 9, 0]
-]
+        int cost = 0;
 
-prim(graph)
+        System.out.println("Edges:");
+
+        while (edges < n - 1) {
+
+            int min = INF;
+
+            int u = -1;
+            int v = -1;
+
+            for (int i = 0; i < n; i++) {
+
+                if (vis[i]) {
+
+                    for (int j = 0; j < n; j++) {
+
+                        if (!vis[j] &&
+                                graph[i][j] != 0 &&
+                                graph[i][j] < min) {
+
+                            min = graph[i][j];
+
+                            u = i;
+                            v = j;
+                        }
+
+                    }
+
+                }
+
+            }
+
+            System.out.println(u + " - " + v + " = " + min);
+
+            vis[v] = true;
+
+            cost += min;
+
+            edges++;
+
+        }
+
+        System.out.println("Total Cost = " + cost);
+
+    }
+}
